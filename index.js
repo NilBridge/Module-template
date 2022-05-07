b4cb54e5950bcfc8622712860ad0337dab85a85f
Sbaoor <70316422+Sbaoor-fly@users.noreply.github.com>
2022-05-07 19:50:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const assert = require('assert');
-
 class template extends NIL.ModuleBase{
 	onStart(api){
 		api.listen('onMainMessageReceived',(e)=>{
 			e.reply(e.raw_message,true);
 		})
 	}
-	moduleName = 'template'
 }
 
 
-assert(template.moduleName != 'temlate','请修改模块名称后删掉此行');
-
-module.exports = new template
+module.exports = new template;
